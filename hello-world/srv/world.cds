8e32fd5d78104801962ecbe107baad38de516507
sovielml <sisi.om@t-online.de>
2023-10-05 17:36:24 +0200

    new services

--- hello-world/srv/world.cds
+++ hello-world/srv/world.cds
@@ -1,3 +1,13 @@
 service say {
     function hello (to: String) returns String;
+    function talktome(who:String, angry:Boolean) returns String;
+    function catfact() returns String;
+    function boredom() returns array of{
+        String activity;
+        String type;
+        String participants;
+        String price;
+        String link;
+        String key;
+    };
 }
--- hello-world/srv/world.js
+++ hello-world/srv/world.js
@@ -1,3 +1,35 @@
+//import {client} from "@gradio/client";
+const cds = require('@sap/cds');
 module.exports = (say) => {
-    say.on('hello',req => `Hello ${req.data.to}!`)
+    say.on('hello', req => `Hello ${req.data.to}.`);
+    say.on('talktome', (req) => { 
+        if (req.data.angry) {
+             return `Fuck off, ${req.data.who}!`
+            }else {
+                return `How can I help you, ${req.data.who}?`
+            }
+    });
+    say.on('catfact',async req=>{
+        const catapi = await cds.connect.to("catfactapi");
+        return catapi.tx(req).get("/fact");
+    });
+    say.on('boredom',async req=>{
+        const boredomapi = await cds.connect.to("boredomapi");
+        let array = boredomapi.tx(req).get("/activity");
+        return array;
+    })
 }
+
+// async function gradio_test(){
+
+//     const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
+//     const exampleImage = await response_0.blob();
+                            
+//     const app = await client("https://sovielml-test.hf.space/");
+//     const result = await app.predict("/predict", [
+//                     exampleImage, 	// blob in 'img' Image component
+//         ]);
+
+//     return(result.data);
+
+// }
