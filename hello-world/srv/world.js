82663b2dde957984d4a839fe06315dfab45575be
sovielml <sisi.om@t-online.de>
2023-10-18 23:30:11 +0200

    gradio is not working currently (sadly)

--- hello-world/srv/world.js
+++ hello-world/srv/world.js
@@ -1,5 +1,5 @@
-const client = require("@gradio/client/package.json");
 const cds = require('@sap/cds');
+
 module.exports = (say) => {
     say.on('hello', req => `Hello ${req.data.to}.`);
     say.on('talktome', (req) => { 
@@ -19,21 +19,6 @@ module.exports = (say) => {
         return array;
     });
     say.on('catordog',async req=>{
-        //return {"label": "Cat", "confidences": [{"label": "Cat", "confidence": 0.9965655207633972}, {"label": "Dog", "confidence": 0.0034344755113124847}]}
-        gradio_test();
+        return {"label": "Cat", "confidences": [{"label": "Cat", "confidence": 0.9965655207633972}, {"label": "Dog", "confidence": 0.0034344755113124847}]}
     })
-}
-
-async function gradio_test(){
-
-    const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
-    const exampleImage = await response_0.blob();
-                            
-    const app = await client("https://sovielml-test.hf.space/");
-    const result = await app.predict("/predict", [
-                    exampleImage, 	// blob in 'img' Image component
-        ]);
-
-    return(result.data);
-
 }
